8c7a345621b050c244145f53944e50432683478f
Apirak Soimaree <apirak.soi@outlook.com>
2025-12-23 01:25:35 +0700

    [category-service] add category repository.

--- src/main/kotlin/com/bigcamera/backend/category/CategoryRepo.kt
+++ src/main/kotlin/com/bigcamera/backend/category/CategoryRepo.kt
@@ -1,5 +1,9 @@
 package com.bigcamera.backend.category
 
-interface CategoryRepo {
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CategoryRepo : JpaRepository<Category, Int> {
 
 }
