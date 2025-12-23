8a9de6b6e263a767a476dc2366cc187f14c55672
Apirak Soimaree <apirak.soi@outlook.com>
2025-12-23 23:41:34 +0700

    [product-service] add product detail repository and add query dsl predicate executor to interface repository product and product detail.

--- src/main/kotlin/com/bigcamera/backend/product/ProductDetailRepo.kt
+++ src/main/kotlin/com/bigcamera/backend/product/ProductDetailRepo.kt
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface ProductRepo : JpaRepository<Product, UUID>, QuerydslPredicateExecutor<Product> {
+interface ProductDetailRepo : JpaRepository<ProductDetail, UUID>, QuerydslPredicateExecutor<ProductDetail> {
 
 }
