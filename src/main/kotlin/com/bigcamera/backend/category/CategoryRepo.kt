39c578bdfb9f5b3fc8fb3234100d6b119e060f6d
Apirak Soimaree <apirak.soi@outlook.com>
2025-12-23 23:42:40 +0700

    [product-service] modified category repository add query dsl predicate executor.

--- src/main/kotlin/com/bigcamera/backend/category/CategoryRepo.kt
+++ src/main/kotlin/com/bigcamera/backend/category/CategoryRepo.kt
@@ -1,9 +1,10 @@
 package com.bigcamera.backend.category
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CategoryRepo : JpaRepository<Category, Int> {
+interface CategoryRepo : JpaRepository<Category, Int>, QuerydslPredicateExecutor<Category> {
 
 }
