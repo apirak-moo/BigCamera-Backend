e29c31a7e9b6de76896b5c9daf59e2b99a696f89
Apirak Soimaree <apirak.soi@outlook.com>
2025-12-23 22:57:41 +0700

    [product-service] implement interface product service to product service.

--- src/main/kotlin/com/bigcamera/backend/product/ProductService.kt
+++ src/main/kotlin/com/bigcamera/backend/product/ProductService.kt
@@ -2,13 +2,32 @@ package com.bigcamera.backend.product
 
 import com.bigcamera.backend.category.CategoryRepo
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class ProductService(
     private val categoryRepo: CategoryRepo,
     private val productRepo: ProductRepo
-) {
+) : IProductService {
 
+    override fun getAllProducts(): List<ProductResponse> {
+        TODO("Not yet implemented")
+    }
 
+    override fun findProductById(id: UUID): ProductResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun createProduct(request: ProductRequest): ProductResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateProduct(id: UUID, request: ProductRequest) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteProduct(id: UUID) {
+        TODO("Not yet implemented")
+    }
 
 }
